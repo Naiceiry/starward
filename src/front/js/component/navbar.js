6a96bdceb275e6070c93151e0a20fe668d329af4
Naiceiry <naiceirymp@gmail.com>
2021-07-27 13:28:48 +0000

    listo, ahora a gregar favoritos

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -7,10 +7,30 @@ export const Navbar = () => {
 			<Link to="/">
 				<img id="startw" src={startw} />
 			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">FAVORITOS O</button>
-				</Link>
+			<div className="btn-group">
+				<button
+					type="button"
+					className="btn btn-danger dropdown-toggle"
+					data-toggle="dropdown"
+					aria-haspopup="true"
+					aria-expanded="false">
+					FAVORITOS
+				</button>
+				<div className="dropdown-menu">
+					<a className="dropdown-item" href="#">
+						Action
+					</a>
+					<a className="dropdown-item" href="#">
+						Another action
+					</a>
+					<a className="dropdown-item" href="#">
+						Something else here
+					</a>
+					<div className="dropdown-divider"></div>
+					<a className="dropdown-item" href="#">
+						Separated link
+					</a>
+				</div>
 			</div>
 		</nav>
 	);
